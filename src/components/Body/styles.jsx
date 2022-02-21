2b3da183282510f499d379fe4fb10bdfc9ef8866
Luis Lozano <lslozano11@gmail.com>
2022-02-20 19:32:18 -0600

    Add styles for Body

---
+++ src/components/Body/styles.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div.attrs({
+  className: 'body-container',
+})`
+  display: flex;
+  flex-direction: column;
+  padding: 25px 10px;
+`;
