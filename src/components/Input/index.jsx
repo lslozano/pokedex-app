4de753cd9dc5e1a393d20b49f1ce3ea1e7ac261e
Luis Lozano <lslozano11@gmail.com>
2022-02-20 19:33:12 -0600

    Add styles for Input

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+// Styles
+import { InputField } from './styles';
 
 const Input = () => {
   return (
     <>
-      <label for="pokemon">Search for a pokemon:</label>
-      <input type="text" name="pokemon" id="pokemon" />
+      <label htmlFor="pokemon"></label>
+      <InputField type="text" name="pokemon" id="pokemon" placeholder="Search for a pokemon" />
     </>
   )
 }
