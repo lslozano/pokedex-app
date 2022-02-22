a70e44e30a22a9839defd14dfd6e0491e86d21f6
Luis Lozano <lslozano11@gmail.com>
2022-02-22 13:02:48 -0600

    Handle search on form

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 // Styles
 import { InputField } from './styles';
 
-const Input = () => {
+const Input = (props) => {
+  const { value, onChange } = props;
+
   return (
     <>
       <label htmlFor="pokemon"></label>
-      <InputField type="text" name="pokemon" id="pokemon" placeholder="Search for a pokemon" />
+      <InputField
+        type="text"
+        name="pokemon"
+        id="pokemon"
+        value={value}
+        placeholder="Search for a pokemon"
+        onChange={(e) => onChange(e)}
+      />
     </>
   )
 }
